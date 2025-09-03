ba463b3d439f8df444124ff7e5b509b8b467cb54
JoseDev <joseslopez08k@gmail.com>
2025-09-03 12:11:26 +0200

    Add Base62 encoding service and URL shortener configuration

---
+++ src/main/java/config/UrlShortenerConfig.java
@@ -0,0 +1,15 @@
+package config;
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+@Component
+@ConfigurationProperties(prefix = "url-shortener")
+@Data
+public class UrlShortenerConfig {
+    private String allowedCharacters;
+    private int keyLength;
+    private String baseUrl;
+}
+
---
+++ src/main/java/service/Base62EncodingService.java
@@ -0,0 +1,32 @@
+package service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class Base62EncodingService {
+
+    private static final String ALLOWED_CHARACTERS = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    private static final int BASE = ALLOWED_CHARACTERS.length();
+
+    public String encode(long id) {
+        StringBuilder encodedString = new StringBuilder();
+
+        while (id > 0) {
+            encodedString.append(ALLOWED_CHARACTERS.charAt((int) (id % BASE)));
+            id = id / BASE;
+        }
+
+        return encodedString.reverse().toString();
+    }
+
+    public long decode(String encodedString) {
+        long decodedNumber = 0;
+
+        for (int i = 0; i < encodedString.length(); i++) {
+            decodedNumber = decodedNumber * BASE +
+                    ALLOWED_CHARACTERS.indexOf(encodedString.charAt(i));
+        }
+
+        return decodedNumber;
+    }
+}
